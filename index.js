0cc49ab58edff8520565d028656629e68e90075e
Hashith Sithuruwan <88393998+Hashith00@users.noreply.github.com>
2024-06-27 08:08:11 +0530

    Update index.js
    Change the port to 3000

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get("/", (req, res) => {
   res.send("hello World");
 });
 
-app.listen(3002, () => console.log("The server is running at PORT " + 3002));
+app.listen(3000, () => console.log("The server is running at PORT " + 3000));
