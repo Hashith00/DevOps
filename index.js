9b29d93220e7d1941782131ee77ed4227b5dab11
Hashith Sithuruwan <88393998+Hashith00@users.noreply.github.com>
2024-06-27 11:51:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get("/", (req, res) => {
   res.send("hello World Hashith");
 });
 
-app.listen(3000, () => console.log("The server is running at PORT " + 3000));
+app.listen(3002, () => console.log("The server is running at PORT " + 3002));
