1ea2efd6726efb28bf603e0a2dbe943c1852eeda
Hashith Sithuruwan <88393998+Hashith00@users.noreply.github.com>
2024-06-27 08:17:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello World");
+  res.send("hello World Hashith");
 });
 
 app.listen(3000, () => console.log("The server is running at PORT " + 3000));
